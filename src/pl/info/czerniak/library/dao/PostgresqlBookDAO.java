883b73febc97268b2e7f9223088060fdf17cc091
Andrzej Czerniak <andrzej.czerniak@gmail.com>
2020-03-09 22:10:12 +0100

    Implement to use Spring JDBC

--- src/pl/info/czerniak/library/dao/PostgresqlBookDAO.java
+++ src/pl/info/czerniak/library/dao/PostgresqlBookDAO.java
@@ -1,71 +1,50 @@
 package pl.info.czerniak.library.dao;
 
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import pl.info.czerniak.library.model.Book;
 import pl.info.czerniak.library.util.ConnectionProvider;
-import pl.info.czerniak.library.util.DbOperationException;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.util.List;
 
 public class PostgresqlBookDAO implements BookDAO{
-    private final static String CREATE = "INSERT INTO book(isbn,title,description) VALUES(?,?,?);";
-    private final static String READ = "SELECT isbn,title,description FROM book WHERE isbn =?;";
-    private final static String UPDATE = "UPDATE book SET isbn=?, title=?, description=? WHERE isbn=?;";
-    private final static String DELETE = "DELETE FROM book WHERE isbn=?;";
+    private final static String CREATE = "INSERT INTO book(isbn,title,description) VALUES(:isbn, :title, :description);";
+    private final static String READ = "SELECT isbn,title,description FROM book WHERE isbn = :isbn;";
+    private final static String UPDATE = "UPDATE book SET isbn= :isbn, title= :title, description= :description WHERE isbn= :isbn;";
+    private final static String DELETE = "DELETE FROM book WHERE isbn= :isbn;";
+
+    private NamedParameterJdbcTemplate template;
+
+    public PostgresqlBookDAO() {
+        template = new NamedParameterJdbcTemplate(ConnectionProvider.getDSInstance());
+    }
 
     public void create(Book book) {
-        try(Connection connection = ConnectionProvider.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(CREATE);){
-            preparedStatement.setString(1,book.getIsbn());
-            preparedStatement.setString(2,book.getTitle());
-            preparedStatement.setString(3,book.getDescription());
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            throw new DbOperationException(e);
-        }
+        BeanPropertySqlParameterSource parameterSource = new BeanPropertySqlParameterSource(book);
+        template.update(CREATE, parameterSource);
     }
 
     public Book read(String isbn) {
         Book resultBook = null;
-        try(Connection connection = ConnectionProvider.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(READ);){
-            preparedStatement.setString(1,isbn);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            if(resultSet.next()){
-                resultBook = new Book();
-                resultBook.setIsbn(resultSet.getString("isbn"));
-                resultBook.setTitle(resultSet.getString("title"));
-                resultBook.setDescription(resultSet.getString("description"));
-            }
-        } catch (SQLException e) {
-            throw new DbOperationException(e);
+        SqlParameterSource parameterSource = new MapSqlParameterSource("isbn",isbn);
+        List<Book> bookList = template.query(READ,parameterSource, BeanPropertyRowMapper.newInstance(Book.class));
+        if(bookList.get(0) != null){
+            resultBook = bookList.get(0);
         }
         return resultBook;
     }
 
     public void update(Book book){
-        try(Connection connection = ConnectionProvider.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(UPDATE);){
-            preparedStatement.setString(1,book.getIsbn());
-            preparedStatement.setString(2,book.getTitle());
-            preparedStatement.setString(3,book.getDescription());
-            preparedStatement.setString(4,book.getIsbn());
-            preparedStatement.executeUpdate();
-        } catch (SQLException e){
-            throw new DbOperationException(e);
-        }
+        BeanPropertySqlParameterSource parameterSource = new BeanPropertySqlParameterSource(book);
+        template.update(UPDATE,parameterSource);
     }
 
     public void delete(Book book){
-        try(Connection connection = ConnectionProvider.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(DELETE);){
-            preparedStatement.setString(1,book.getIsbn());
-            preparedStatement.executeUpdate();
-        } catch (SQLException e){
-            throw new DbOperationException(e);
-        }
+        SqlParameterSource parameterSource = new MapSqlParameterSource("isbn", book.getIsbn());
+        template.update(DELETE,parameterSource);
     }
 
 }
